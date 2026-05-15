c2bf4149e0dab3ff773cc7915f3a3d57b522affd
Blaji <johnbalaji786@gmail.com>
2026-05-15 15:54:30 +0530

    server setup --done

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
 const { MONGODB_URI } = require('./utils/config,js');
+const { PORT } = require('./utils/config,js');
+const app = require('./app');
 
 mongoose
     .connect(MONGODB_URI)
     .then(() => {
         console.log('Connected to MongoDB');
+        app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+     });
+
     })
     .catch((err) => {
         console.error('Failed to connect to MongoDB', err);
