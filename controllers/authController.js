69d8cf5a5690de745d3615634a74e2975569b34f
Blaji <johnbalaji786@gmail.com>
2026-07-07 20:15:04 +0530

    fix: correct user model import

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-const User = require("../models/user");
+const User = require("../models/ser");
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET, NODE_ENV } = require('../utils/config');
