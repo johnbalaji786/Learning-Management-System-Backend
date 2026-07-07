fa34cb6e12f4cdbb6f19db2a4ce5766b30ba2f5f
Blaji <johnbalaji786@gmail.com>
2026-07-07 21:59:22 +0530

    frontend link updated

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const cors = require("cors");
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://learnhubgrow.netlify.app",
     credentials: true,
   }),
 );
